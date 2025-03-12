cda1fd5aead45e779083e4dee442e1b10ed6c87d
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-03-12 15:07:09 +0800

    Minor Improvements

--- pages/agency.vue
+++ pages/agency.vue
@@ -12,3 +12,14 @@
         </main>
     </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Agency',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
