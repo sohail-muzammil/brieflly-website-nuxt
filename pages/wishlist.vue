35b6fa7567dd8795425fc5dbea83733d06573a37
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-03-16 15:07:43 +0800

    Minor Improvements

--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -7,3 +7,14 @@
         </main>
     </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Join our exclusive waitlist',
+  ogTitle: 'Join our exclusive waitlist',
+  description: 'Get early access to Brieflly—where businesses connect seamlessly with top-tier agencies.',
+  ogDescription: 'Get early access to Brieflly—where businesses connect seamlessly with top-tier agencies.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
